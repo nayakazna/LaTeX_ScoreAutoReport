9cc5d54e5001f41fb5053d563a9a5123343ff5e6
Nayaka <19623116@mahasiswa.itb.ac.id>
2025-01-28 15:13:40 +0700

    feat: integrate email distribution (send_email()) to main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-# from .modules.email import *
-from modules import latex
+from modules import emailutils as email
+from modules import latexutils as latex
 import csv
+
+your_email = input('Enter your email: ')
+your_password = input('Enter your password: ')
 
 with open('data.csv', 'r') as file:
     # Data preparation
@@ -23,7 +26,14 @@ with open('data.csv', 'r') as file:
         email_address = row[2]
         scores = row[3:]
 
+        ### Generate PDF
         latex.add_identity(name, nim)
         latex.add_table(score_nickname, score_weight, scores)
         latex.compile_latex(nim)
 
+        ### Send email
+        subject = 'Rincian Nilai IF2123 Aljabar Linear dan Geometri' # Customize the email subject here
+        body = f'Berikut adalah rincian nilai Anda untuk mata kuliah IF2123 Aljabar Linear dan Geometri.\n\nTerima kasih.' # Customize the email body here
+        attachment_path = f'outputs/{nim}.pdf'
+        attachment_filename = f'{nim}.pdf'
+        email.send_email(your_email, your_password, email_address, subject, body, attachment_path, attachment_filename)
