6b42f47bf0e5d186e8f10d4b3f0d49a1265f0dca
Nayaka <19623116@mahasiswa.itb.ac.id>
2025-01-28 15:12:56 +0700

    feat: finalize send_email()

--- modules/emailutils.py
+++ modules/emailutils.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.message import EmailMessage
 
-def send_email(sender_email, sender_password, receiver_email, subject, body, attachment=None):
+def send_email(sender_email, sender_password, receiver_email, subject, body, attachment_path=None, attachment_filename=None):
     # Sends an email with attachment
     ## Setup the MIME
     msg = EmailMessage()
@@ -9,12 +9,22 @@ def send_email(sender_email, sender_password, receiver_email, subject, body, att
     msg['To'] = receiver_email
     msg['Subject'] = subject
     msg.set_content(body)
-    msg.add_attachment(attachment)
+
+    ## Attach the attachment if exists
+    if (attachment_path != None):
+        with open(attachment_path, 'rb') as f:  # Open in binary read mode
+            msg.add_attachment(
+                f.read(),                   # Content of the attachment
+                maintype='application',     # MIME type category
+                subtype='pdf',              # MIME type subtype
+                filename=attachment_filename# Attachment name
+            )
     
     ## Send the email
     try:
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
             server.login(sender_email, sender_password)
             server.send_message(msg)
+            print(f'Email sent to {receiver_email}')
     except Exception as e:
         print(e)
